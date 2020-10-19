94882b99eda2beed94ecf64d65f345b85e0d6bf4
Akasho2h <akash.sharma@o2h.com>
2020-10-19 16:37:40 +0530

    react table added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import React from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
 import store from './Redux/store'
-import Myform from './Form'; 
+// import Myform from './Form'; 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import BasicTable from './Table/BasicTable';
 
 function App(props) {
 
     // console.log(props);
     return (
         <Provider store={store}>
-            <Myform/>
+            {/* <Myform/> */}
+            <BasicTable/>
         </Provider>
     );
 }
